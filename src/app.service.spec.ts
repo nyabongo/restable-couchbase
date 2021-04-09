701c9e9fb009d4854c15e2aae37ff54c2fce4db4
Angello Obel <angello@obel.co>
2021-04-09 13:38:41 +0300

    Added stub Service

---
+++ src/app.service.spec.ts
@@ -0,0 +1,13 @@
+import { IRestableService } from '@obel/restable';
+import {AppService} from './app.service'
+
+
+describe('AppService', () => {
+    let appService:IRestableService
+    beforeEach(() => {
+        appService = new AppService();
+    });
+    it('should do stuff', () => {
+        fail("Nothing implemented ye t ")
+    });
+});
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { AttributeType, IRestableService, ResourceListResponse, ResourceResponse } from '@obel/restable'
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements IRestableService {
+  createItem(type: string, body: AttributeType): Promise<ResourceResponse> {
+    throw new Error('Method not implemented.');
+  }
+  findById(type: string, id: string): Promise<ResourceResponse> {
+    throw new Error('Method not implemented.');
+  }
+  editItem(type: string, id: string, attributes: AttributeType): Promise<ResourceResponse> {
+    throw new Error('Method not implemented.');
+  }
+  getItemsByType(type: string): Promise<ResourceListResponse> {
+    throw new Error('Method not implemented.');
   }
 }
